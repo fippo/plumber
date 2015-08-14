8914bc354c8a0ed21036bef9c5ff728ed70ba6fd
Philipp Hancke <fippo@andyet.net>
2015-08-14 10:50:03 -0700

    more doorhanger questions, changed order

--- index.js
+++ index.js
@@ -106,14 +106,15 @@ webrtcUI.receiveMessage = function(msg) {
             });
             var browserWindow = msg.target.ownerDocument.defaultView;
             //console.log('BWIN', browserWindow);
+            
             console.log(msg.target);
             browserWindow.PopupNotifications.show(msg.target,
                 'webrtc-datachannel',
-                'Allow WebRTC P2P networking',
+                'Allow WebRTC P2P networking for ' + origin,
                 null,
                 {
-                    label: 'Yes',
-                    accessKey: 'y',
+                    label: 'Allow',
+                    accessKey: 'a',
                     callback: function() {
                         windows[origin].hasPCPermission = true;
                         windows[origin].pending.forEach(function (request) {
@@ -128,8 +129,8 @@ webrtcUI.receiveMessage = function(msg) {
                 },
                 [
                     {
-                        label: 'No',
-                        accessKey: 'n',
+                        label: 'Deny',
+                        accessKey: 'd',
                         callback: function() {
                             console.log('no');
                             windows[origin].hasPCPermission = false;
@@ -143,6 +144,60 @@ webrtcUI.receiveMessage = function(msg) {
                                 });
                             });
                             windows[origin].pending = [];
+                        }
+                    },
+                    {
+                        label: 'Always allow',
+                        accessKey: 'A',
+                        callback: function() {
+                            // FIXME: copy-paste currently
+                            windows[origin].hasPCPermission = true;
+                            windows[origin].pending.forEach(function (request) {
+                                console.log('pending', request);
+                                request.messageManager.sendAsyncMessage('rtcpeer:Allow', {
+                                    callID: request.callID,
+                                    windowID: request.windowID
+                                });
+                            });
+                            windows[origin].pending = [];
+                            // FIXME: persist
+                        }
+                    },
+                    {
+                        label: 'Always deny when no camera permission is asked',
+                        accessKey: 'w',
+                        callback: function() {
+                            console.log('no');
+                            windows[origin].hasPCPermission = false;
+                            // this revokes the GUM grant currently just to be on the safe side
+                            windows[origin].hasGUMPermission = false;
+                            windows[origin].pending.forEach(function (request) {
+                                console.log('pending', request);
+                                request.messageManager.sendAsyncMessage('rtcpeer:Deny', {
+                                    callID: request.callID,
+                                    windowID: request.windowID
+                                });
+                            });
+                            windows[origin].pending = [];
+                        }
+                    },
+                    {
+                        label: 'Always deny',
+                        accessKey: 'D',
+                        callback: function() {
+                            console.log('no');
+                            windows[origin].hasPCPermission = false;
+                            // this revokes the GUM grant currently just to be on the safe side
+                            windows[origin].hasGUMPermission = false;
+                            windows[origin].pending.forEach(function (request) {
+                                console.log('pending', request);
+                                request.messageManager.sendAsyncMessage('rtcpeer:Deny', {
+                                    callID: request.callID,
+                                    windowID: request.windowID
+                                });
+                            });
+                            windows[origin].pending = [];
+                            // FIXME: persist
                         }
                     }
                 ]
